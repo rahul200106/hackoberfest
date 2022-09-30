9fed5f244fbadf9b591b368d60a70b991699120c
purnendra19 <92633709+purnendra19@users.noreply.github.com>
2022-10-01 00:30:55 +0530

    Add files via upload

---
+++ Invertedhalfpyramd180.java
@@ -0,0 +1,30 @@
+// This code print following code
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+import java.util.*;
+public class Invertedhalfpyramd180 {
+    public static void main (String args []){
+        System.out.println("ENTER A VALUE OF N ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt()
+        //outer loop
+        for(int i=1; i<=n; i++){
+            //1st inner loop for printing space " "
+            for(int j=1; j>=n-i;j++){
+                System.out.print(" ");
+            }
+            //2ed inner loop for printing star "*"
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
---
+++ Rectangle.java
@@ -0,0 +1,28 @@
+// This code print a rectangle box.
+// y AXIS = length of rectangle box.
+// x AXIS = width of rectangle box.
+
+// code look iike this.
+
+// whan y AXIS is 4 and x AXIS 5.
+// ******
+// ******
+// ******
+// ******
+
+import java.util.*;
+public class Rectangle{
+    public static void main (String args []){
+        System.out.println("ENTER y AXIS VALUE")
+        Scanner sc = new Scanner(System.in); 
+        int n = sc.nextInt();
+        System.out.println("ENTER x AXIS VALUE");
+        int m = sc.nextInt();
+        for(int i=1; i>=n;i++){
+            for(int j=1; j<=m;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }sc.close();
+}
+}
