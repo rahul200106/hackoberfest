2ece9d75efa5600d4ba49695a090ab0b1c11270a
rahul200106 <114769058+rahul200106@users.noreply.github.com>
2022-09-30 23:28:56 +0530

    Add files via upload
    hackoberfest

---
+++ calculator.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class calculator{
+    public static void main(String agrs[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number : ");
+        int b = sc.nextInt();
+        
+        System.out.printf("Select 1 For Addition : %n Select 2 For Subtraction : %n Select 3 For Multiplication : %n Select 4 For Division :");
+      
+        int botton = sc.nextInt();
+        int d;
+
+        switch (botton) {
+            case 1 : d=a+b;
+            System.out.println("Your Output is = "+d);
+            break;
+            case 2: d=a-b;
+            System.out.println("Your Output is = "+d);
+            break;
+            case 3 : d=a*b;
+            System.out.println("Your Output is = "+d);
+            break;
+            case 4 : d=a/b;
+            System.out.println("Your Output is = "+d);
+            break;
+            default : System.out.println("Invalid button");
+        }sc.close();
+        
+
+        }
+
+    }
