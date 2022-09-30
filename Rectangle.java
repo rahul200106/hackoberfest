a038b181820de66815e529a70ff01eac70970fbd
Rahul90474 <110100790+narnolii@users.noreply.github.com>
2022-10-01 00:35:20 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -13,12 +13,12 @@
 import java.util.*;
 public class Rectangle{
     public static void main (String args []){
-        System.out.println("ENTER y AXIS VALUE")
+        System.out.println("ENTER y AXIS VALUE");
         Scanner sc = new Scanner(System.in); 
         int n = sc.nextInt();
         System.out.println("ENTER x AXIS VALUE");
         int m = sc.nextInt();
-        for(int i=1; i>=n;i++){
+        for(int i=1; i<=n;i++){
             for(int j=1; j<=m;j++){
             System.out.print("*");
         }
